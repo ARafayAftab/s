00bf84d643fae874f35c6ffc7f4f28f31f0e2968
rafay <rafayaftab2007@gmail.com>
2025-01-25 17:54:47 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var fan = {
+      wings: 6,
+      color: "white",
+     size: "smalle",
+     type: "stand",
+      isRunning: false,
+       speeds: ["1", "2", "3"],
+       brandDetails: {
+         name: "Pak Fan",
+        established: 1998,
+      }
+    }
+    console.log(fan);
+    
+
+    var fruits = ["Apple", "Banana", "Mango", "Pear"];
+    
+    console.log(fruits[2], fruits[1]);
+    
+     console.log(fan.speeds[2]);
+     console.log(fan.brandDetails);
